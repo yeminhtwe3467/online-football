03b9fb2ded9bb08f20bda9ce490388b599784a14
Ye Min Htwe <yeminhtwe@Yes-MacBook-Pro.local>
2025-03-30 23:57:45 +0630

    ll

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Update apt repositories
 apt-get update
 
+# Install necessary dependencies for Playwright (without root)
+apt-get install -y wget fonts-liberation libappindicator3-1 libnss3 libxss1 libxtst6 xdg-utils
+
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
