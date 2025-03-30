71e728098b5a50e89c4a83674bc855dd3497e287
Ye Min Htwe <yeminhtwe@Yes-MacBook-Pro.local>
2025-03-30 23:17:35 +0630

    final updated

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir -p /home/render/.cache  # Ensure the cache directory exists
 pip install -r requirements.txt  # Install dependencies
 playwright install  # Install Playwright browsers
