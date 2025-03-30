120e2451f39b3461266812dc9d20c59706620035
Ye Min Htwe <yeminhtwe@Yes-MacBook-Pro.local>
2025-03-30 22:33:34 +0630

    updated

--- api.py
+++ api.py
@@ -1,17 +1,24 @@
+import os
 from fastapi import FastAPI
 from playwright.async_api import async_playwright
 
 app = FastAPI()
 
+# Load environment variables
+USER_AGENT = os.getenv("SCRAPER_USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+
 async def scrape_data():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, args=["--no-sandbox", "--disable-setuid-sandbox"])
+        browser = await p.chromium.launch(
+            headless=True, 
+            args=["--no-sandbox", "--disable-setuid-sandbox"]
+        )
         page = await browser.new_page()
 
-        '''await page.set_extra_http_headers("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")'''
+        # Set the User-Agent dynamically from environment variables
+        await page.set_extra_http_headers({"User-Agent": USER_AGENT})
 
         await page.goto("https://socolivesq.com/", timeout=120000)
-
         await page.wait_for_selector(".hot-content.all-content li", timeout=25000)
 
         scraped_data = await page.evaluate('''
